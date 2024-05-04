27951a34ddc53ca4130b19eb7fd393dcf11a2795
cling <10439500+one-finger-shenman_0@user.noreply.gitee.com>
2024-05-04 12:43:59 +0800

    修改了更新工具架方式

--- download/new.py
+++ download/new.py
@@ -6,52 +6,63 @@
 # AI聊天绘画：ai.cgfml.com
 # 敲码不易修改搬运请保留以上信息，感谢！！！！！
 import os
-import shutil
-import urllib.request
-import zipfile
-import json
-from PySide2 import QtWidgets
+import sys
 
-bbh = 3.0
-destination_folder = 'D:/Cling_toolbox'
-bbh_json_url = "https://tool.cgfml.com/Cling_toolbox_3.0/bbh.json"
-download_url = "https://tool.cgfml.com/Cling_toolbox_3.0/Cling_toolbox.zip"
-temp_zip_file = os.path.join(destination_folder, "Cling_toolbox.zip")
+sys.path.append('D:\\cling_toolbox\\external_library')
 
-if not os.path.exists(destination_folder):
-    os.makedirs(destination_folder)
+from git import Repo
+from maya import cmds
 
-try:
-    url = urllib.request.urlopen(bbh_json_url)
-    bbh_json_data = json.loads(url.read())
-    if bbh != bbh_json_data["version"]:
-        cmds.warning(u"当前版本（{}）最新版本（{}）".format(bbh, bbh_json_data["version"]))
 
-        # 创建简单的对话框
-        msgBox = QtWidgets.QMessageBox()
-        msgBox.setWindowTitle(u"更新提示")
+def pull_updates(repo):
+    # 从远程仓库拉取更新
+    origin = repo.remotes.origin
+    origin.pull()
+    # 更新成功后的确认对话框
+    cmds.confirmDialog(title='Update Successful',
+                       message='更新已完成，请重启工具架享用！',
+                       button=['OK'], defaultButton='OK', cancelButton='OK', dismissString='OK')
+    # 关闭窗口
+    cmds.deleteUI('UpdateWindow', window=True)
 
-        description = bbh_json_data.get("description", "")
-        release_date = bbh_json_data.get("release_date", "")
-        formatted_description = description.replace("\\n", "\n")
-        msgBox.setText(u"当前版本（{}）最新版本（{}）\n描述：{}\n发布日期：{}\n是否更新？".format(bbh, bbh_json_data["version"], formatted_description, release_date))
-        msgBox.setStandardButtons(QtWidgets.QMessageBox.Yes | QtWidgets.QMessageBox.No)
-        choice = msgBox.exec_()
 
-        if choice == QtWidgets.QMessageBox.Yes:
-            urllib.request.urlretrieve(download_url, temp_zip_file)
-            with zipfile.ZipFile(temp_zip_file, 'r') as zip_ref:
-                zip_ref.extractall(destination_folder)
+def check_for_updates(repo_path, remote_url):
+    if not os.path.exists(repo_path):
+        # 如果本地仓库不存在，就克隆远程仓库
+        Repo.clone_from(remote_url, repo_path)
 
-            cmds.confirmDialog(title=u"更新完成", message=u'成功更新，请重启maya，请勿删除 "{}"'.format(destination_folder), button=[u"确定"])
-        else:
-            print(u"未进行更新操作")
+    repo = Repo(repo_path)
+    origin = repo.remotes.origin
+
+    # 获取当前的commit
+    current_commit = repo.head.commit
+
+    # 从远程仓库获取最新的信息
+    origin.fetch()
+
+    # 获取最新的commit
+    latest_commit = origin.refs.master.commit
+
+    # 获取两个commit之间的差异
+    diffs = current_commit.diff(latest_commit)
+
+    if not diffs:
+        # 如果没有差异，说明已经是最新版本
+        cmds.confirmDialog(title='No Updates',
+                           message='当前已经是最新版啦！',
+                           button=['OK'], defaultButton='OK', cancelButton='OK', dismissString='OK')
     else:
+        # 如果有差异，说明有更新
+        def update_dialog():
+            cmds.window('UpdateWindow', title='Update Available', widthHeight=(200, 60))
+            cmds.columnLayout(adjustableColumn=True)
+            cmds.text(label='Update Available', align='center')
+            cmds.text(label='Latest commit message:', align='left')
+            cmds.text(label=latest_commit.message, align='left')
+            cmds.button(label='Update', command=lambda *args: pull_updates(repo))
+            cmds.showWindow()
 
-        cmds.confirmDialog(title=u"更新提醒", message=u"你已经是最新版无需更新。", button=[u"确定"])
+        update_dialog()
 
-except Exception as e:
-    print(u'下载和解压文件时发生错误请联系cling: {}'.format(e))
-finally:
-    if os.path.exists(temp_zip_file):
-        os.remove(temp_zip_file)
+
+check_for_updates('D:\\cling_toolbox', 'https://gitee.com/one-finger-shenman_0/cling_toolbox.git')
