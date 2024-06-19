4f7152f9d581833587cbb2d7fffc9c2bd572e0d0
cling <10439500+one-finger-shenman_0@user.noreply.gitee.com>
2024-06-19 23:45:37 +0800

    更新一键安装cmd显示bug

--- 一键安装.bat
+++ 一键安装.bat
@@ -1,20 +1,26 @@
 @echo off
+chcp 65001 >nul
 setlocal
-
 
 set "getProgramName=Cling_toolbox"
 set "getAimDir=D:\Cling_toolbox"
 set "getMayaModDir=%USERPROFILE%\Documents\maya\"
 
-
 if not exist "%getMayaModDir%modules\" (
-    mkdir "%getMayaModDir%modules\"
+    mkdir "%getMayaModDir%modules\" || (
+        echo 创建目录 %getMayaModDir%modules\ 失败。
+        pause
+        exit /b 1
+    )
 )
-
 
 (
     echo + %getProgramName% 1.0 %getAimDir%
-) > "%getMayaModDir%modules\%getProgramName%.mod"
+) > "%getMayaModDir%modules\%getProgramName%.mod" || (
+    echo 创建工具文件 %getMayaModDir%modules\%getProgramName%.mod 失败。
+    pause
+    exit /b 1
+)
 
-
-echo Maya module for %getProgramName% has been created.
+echo Maya工具 %getProgramName% 已成功创建。
+pause
